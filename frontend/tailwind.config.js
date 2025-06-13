60f6df2da9259e4e899e3192bafa85ffe79e006e
Nicolas Boites <234995@ust.com>
2025-06-13 13:25:27 -0600

    Navbar basic structure and styling

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", // ajusta según tus archivos
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,html}"], // Asegúrate de que los paths sean correctos
+
   theme: {
     extend: {},
   },
