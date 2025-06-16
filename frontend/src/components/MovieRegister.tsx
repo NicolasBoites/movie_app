2178c173ba679b9c1f64f007afb1794deb7ee079
Rodolfo valtierra <rivd@outlook.es>
2025-06-15 20:43:34 -0600

    ending movie creator

--- frontend/src/components/MovieRegister.tsx
+++ frontend/src/components/MovieRegister.tsx
@@ -1,7 +1,34 @@
 import { Heading, Text, IconButton, Flex, Box, TextField, Select, Card, Grid, Button } from '@radix-ui/themes'
 import { ArrowLeftIcon, PlusIcon, InfoCircledIcon } from '@radix-ui/react-icons'
+import { useState, SyntheticEvent } from 'react'
+import Movie from '../fetchs/movies'
+import {useNavigate} from 'react-router'
+import type IMovie from '../interfaces/movies'
 
 export default function () {
+	const [movie, setMovie]:any = useState<IMovie>({});
+	const [errors, setErrors] = useState<IMovie>({});
+	const navigate = useNavigate();
+
+	const saveData = (event: SyntheticEvent) => {
+		const el = event.target as HTMLInputElement;
+		setMovie(old => ({...old, [el.name]: el.value}));
+	}
+
+	const selectGenre = (value: int) => {
+		setMovie({...movie, genre: value});
+	}
+
+	const saveMovie = () => {
+		Movie.post(movie)
+		.then(res => {
+			if (res.error)
+				navigate('/')
+		}).catch(async err => {
+			setErrors(await err)
+		})
+	}
+
     return <Grid columns="1" gap="2" style={{ justifyItems: 'center', }} align="center" mt="9">
         <Grid gap="5" align="center">
             <Flex align="center" gap="4" >
@@ -18,13 +45,13 @@ export default function () {
                 </Grid>
             </Flex>
 						<Card>
-							<Grid gap="4">
+							<Grid gap="4" m="4">
 								<Text size="2">MOVIE TITLE</Text>
-								<TextField.Root color="gray" variant="surface"  size="3" placeholder="Enter movie title..." />
+								<TextField.Root name="title" color="gray" onChange={saveData} variant="surface"  size="3" placeholder="Enter movie title..." />
 								<Text size="2">RANK</Text>
-								<TextField.Root color='gray' variant="surface" size="3"  placeholder="Enter rank number..." />
+								<TextField.Root name="rank" color='gray' onChange={saveData} variant="surface" size="3"  placeholder="Enter rank number..." />
 								<Text size="2">Genre</Text>
-								<Select.Root size="3" >
+								<Select.Root size="3" name="genre" onValueChange={selectGenre}>
 										<Select.Trigger placeholder='Select genre...' color='gray' variant="surface"  />
 										<Select.Content color="gray" variant="solid">
 												<Select.Item value="0">
@@ -42,11 +69,11 @@ export default function () {
 										</Select.Content>
 								</Select.Root>
 							</Grid>
-							<Grid columns="2" gap="3" justify="center" mt="6">
+							<Grid columns="2" gap="3" justify="center" m="4" mt="6">
 								<Button radius="none" size="4" variant="outline" color='gray' highContrast>
 											Cancel
 									</Button>
-									<Button radius="none" size="4" color='gray' highContrast>
+									<Button onClick={saveMovie} radius="none" size="4" color='gray' highContrast>
 											<PlusIcon />
 											Create Movie
 									</Button>
---
+++ frontend/src/interfaces/movies.ts
@@ -0,0 +1,7 @@
+
+export default interface {
+	id?: string,
+	title: string,
+	rank: Number,
+	genre: string
+}
