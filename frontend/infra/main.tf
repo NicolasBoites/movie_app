52ca1ff18dd7b3a3020e4a11491c9a32e4ee6be5
Nicolas Boites <nickboites@gmail.com>
2025-06-19 17:35:25 -0600

    Remove setting bucket policy

--- frontend/infra/main.tf
+++ frontend/infra/main.tf
@@ -83,10 +83,7 @@ data "aws_iam_policy_document" "public_read" {
   }
 }
 
-resource "aws_s3_bucket_policy" "movie_app_frontend" {
-  bucket = aws_s3_bucket.movie_app_frontend.id
-  policy = data.aws_iam_policy_document.public_read.json
-}
+
 
 output "bucket_name" {
   value = aws_s3_bucket.movie_app_frontend.bucket
