9521fb68dbc9e6079c1ffbc2b801ae0d0f572a47
Nicolas Boites <nickboites@gmail.com>
2025-06-20 10:39:21 -0600

    Connect favorites service locally. Remove from favorites

--- frontend/src/favorites/FavoriteList.tsx
+++ frontend/src/favorites/FavoriteList.tsx
@@ -3,15 +3,25 @@ import FavoriteCard from "./FavoriteCard";
 
 interface FavoriteListProps {
   favorites: Favorite[];
+  onRefresh?: () => void; // Callback para refrescar la lista
 }
 
-function FavoriteList({ favorites }: FavoriteListProps) {
+function FavoriteList({ favorites, onRefresh }: FavoriteListProps) {
+  const handleRemoveFavorite = () => {
+    // Refrescar la lista después de remover
+    if (onRefresh) {
+      onRefresh();
+    }
+  };
 
   return (
     <div className="flex flex-col space-y-5 my-12">
       {favorites.map((favorite) => (
         <div className="w-full" key={favorite.id}>
-          <FavoriteCard favorite={favorite} />
+          <FavoriteCard 
+            favorite={favorite} 
+            onRemove={handleRemoveFavorite}
+          />
         </div>
       ))}
     </div>
